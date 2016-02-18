e69727e9bd7976a95646aa9e3f375e4c49a9ac92
Fabiano Matos <fabiano.matos@ifnmg.edu.br>
2016-02-18 15:27:40 -0200

    novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # default root
   root 'pages#index'
   
   get 'diarias' => 'pages#diarias' 
+  get 'servidores' => 'pages#servidores'
+  get 'sobre' =>  'pages#sobre'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
