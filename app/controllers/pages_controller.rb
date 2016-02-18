88eed1437689090edda365b15f05a0f749552038
Fabiano Matos <fabiano.matos@ifnmg.edu.br>
2016-02-18 15:07:08 -0200

    melhorias na view diarias

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
     
   end
   
+  def diarias
+    @top10 = Diaria.select(:valor).where(:data => '01/2015'..'12/2015').group(:nome).order('sum_valor DESC').limit(10).sum(:valor)
+  end
+  
+  def servidores
+    @servidores = Servidor.all.order(:nome)
+  end
+  
 end
