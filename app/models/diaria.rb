8995a89914ed43054f6b4856520bab8d4e944748
Fabrício Matos <fgm2@cin.ufpe.br>
2016-04-20 15:37:48 -0300

    Criando def para calcular o total de diarias geral ou de um ano especifico.

--- app/models/diaria.rb
+++ app/models/diaria.rb
@@ -1,2 +1,11 @@
 class Diaria < ActiveRecord::Base
+  
+  def self.TotalDiarias(ano = nil)
+    if ano.nil?
+      self.select(:valor).order('sum_valor DESC').sum(:valor)
+    else
+      self.select(:valor).where("data BETWEEN ? AND ? ", ano.to_s + "-01-01", ano.to_s + "-12-31").order('sum_valor DESC').sum(:valor)
+    end
+  end
+  
 end
