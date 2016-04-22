b64f2821936cd8f4efd4b5c4caa639103970b47f
Fabiano Matos <fabiano.matos@live.com>
2016-04-22 14:59:09 -0300

    atualizada importacao de dados

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,49 +3,54 @@ require "import"
 
 # DataImport.new(type, year_range, month_range, options = nil)
 
-  # => type: dado a importar: "diarias" ou "servidores"
+# tasks - list of task to import
+  # taskname: um nome para identificar a tarefa. Ex.: :task01, ou :importando2015
+  # => type: dado a importar: ":diarias" ou ":servidores"
   # => year_range: intevalo de anos. Ex: 2011..2015
   # => month_range: intervalo de meses. Ex: 1..2  ou :all para todos os meses
   # => options: { } - Paramentros para leitura do CSV
   # => filter_colums - colunas no CSV para filtrar os dados
 
-import_conf = { 
-        :diarias => { 
+
+tasks = { 
+        :t01 => {
+            :type => :diarias, 
             :year_range => 2011..2015, 
-            :month_range => :all #01..01 
+            :month_range => :all # equivalente a 1..12 
         }, 
-        :servidores => { 
-            :year_range => 2015..2015, 
-            :month_range => 01..01 #[01, 10, 11, 12]  
+        :t02 => {
+            :type => :diarias, 
+            :year_range => 2016, 
+            :month_range => 01..03
+        }, 
+        :t03 => { 
+            :type => :servidores, 
+            :year_range => 2014..2015, 
+            :month_range => :all
+        },
+        :t04 => {
+            :type => :servidores, 
+            :year_range => 2016, 
+            :month_range => 1..3
+            # ,
+            # :options => {:quote_char => "|"}
         } 
   }
+  
+# to log the execution uncomment the following lines
+# $stdout = File.new("import-log#{Time.new}.log","w")
+# $stdout.sync = true
 
-import_conf.each do | type, options |
-    puts "Deseja importar dados de '" + type.to_s.upcase + "' do IFNMG ? (y/n) ou (s/n)"
+# A cada TASK | key, value |
+tasks.each do | task, options |
+    puts "##{task}-> Deseja importar dados de '#{options[:type].upcase}' (ANOS->#{options[:year_range]}) (MESES->#{options[:month_range]}) do IFNMG ? (y/n) ou (s/n)"
     choice = STDIN.gets.chomp
     if (choice.upcase == "Y" || choice.upcase == "S")
-      import = DataImport.new(type, options[:year_range], options[:month_range])
-      import.execute
-      # import.run_test
+      import = DataImport.new(options[:type], options[:year_range], options[:month_range], options[:options])
+      # import.execute
+      import.run_test
     end
 end
-
-# puts "Deseja importar dados de DIARIAS do IFNMG ? (y/n) ou (s/n)"
-# resposta = STDIN.gets.chomp
-# 
-# if (resposta.upcase == "Y" || resposta.upcase == "S")
-  # diarias = DataImport.new(:diarias, 2011..2014, :all)
-  # diarias.execute
-# end
-# 
-# puts "Deseja importar dados de SERVIDORES do IFNMG ? (y/n) ou (s/n)"
-# resposta = STDIN.gets.chomp
-# 
-# if (resposta.upcase == "Y" || resposta.upcase == "S")
-  # servidores = DataImport.new(:servidores, 2015..2015, 1..1)
-  # servidores.execute
-# end
-  
 
 # # This file should contain all the record creation needed to seed the database with its default values.
 # # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
