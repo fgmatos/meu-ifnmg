959f877ffc9dae8e82aaba6a1c5faa9257704c87
Fabiano Matos <fabiano.matos@live.com>
2016-05-01 17:48:27 -0300

    criando metodos 'helpers' e removendo arquivo nao utilizado

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,20 @@ module ApplicationHelper
     doc.to_html.html_safe
   end
   
+  # return a span tag with bootstrap glyphicon image
+  def icon(name, css_class = nil)
+    content_tag(:span,"", :class => "glyphicon glyphicon-#{name} #{css_class}")
+  end
+  
+  def n(number, options=nil)
+    
+    default = {separator: ",", delimiter: ".", unit: ""}
+    
+    if (!options.nil?)
+      default.merge!(options)          
+    end
+
+    number_to_currency(number, default) 
+  end
+  
 end
