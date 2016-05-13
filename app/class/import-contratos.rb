7c917611f2fac3f77d45bee3e06e50b4b1fc3309
Fabiano Matos <fabiano.matos@live.com>
2016-05-13 13:48:51 -0300

    alteradas confs para rodar o sistema em modo de producao

--- app/class/import-contratos.rb
+++ app/class/import-contratos.rb
@@ -7,30 +7,30 @@ require 'contrato.rb'
 
 link_base = "http://www3.transparencia.gov.br/TransparenciaPublica/jsp/contratos/"
 
-contratos ||= APIGov.new(:contratos)
-@contratos ||= contratos.getContratos
-
-@contratos.each do | num, valores |
-  c = Contrato.new
-  
-  c.numero = valores[:numero]
-  c.ano = valores[:numero].split("/").last
-  c.modalidade = valores[:modalidade]
-  c.situacao = valores[:situacao]
-  c.contratado = valores[:contratado]
-  c.contratado_cnpj = valores[:contratado].split(" - ").first
-  
-  # c.contratado_nome
-  
-  c.unidade = valores[:unidade]
-  c.unidade_id = valores[:unidade].split(" - ").last
-  c.unidade_nome = valores[:unidade].split(" - ").first
-  
-  c.objeto = valores[:objeto]
-  
-  c.link = (l.to_s + valores[:link_detalhes].to_s.split('"').second.gsub("amp;", "") )
-  c.ref = num+1 
-  
-  c.save!
-  
-end
+# contratos ||= APIGov.new(:contratos)
+# @contratos ||= contratos.getContratos
+# 
+# @contratos.each do | num, valores |
+  # c = Contrato.new
+#   
+  # c.numero = valores[:numero]
+  # c.ano = valores[:numero].split("/").last
+  # c.modalidade = valores[:modalidade]
+  # c.situacao = valores[:situacao]
+  # c.contratado = valores[:contratado]
+  # c.contratado_cnpj = valores[:contratado].split(" - ").first
+#   
+  # # c.contratado_nome
+#   
+  # c.unidade = valores[:unidade]
+  # c.unidade_id = valores[:unidade].split(" - ").last
+  # c.unidade_nome = valores[:unidade].split(" - ").first
+#   
+  # c.objeto = valores[:objeto]
+#   
+  # c.link = (l.to_s + valores[:link_detalhes].to_s.split('"').second.gsub("amp;", "") )
+  # c.ref = num+1 
+#   
+  # c.save!
+#   
+# end
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
+
+RAILS_ENV = 'production'
+
 Rails.application.initialize!
